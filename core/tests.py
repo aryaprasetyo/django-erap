92bd54d91db56b74f7e13cf33c9ba26e57f7aa88
aryaadhari <arya@mai.com>
2025-04-07 12:45:37 +0700

    feat: add integration test

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class DashboardViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_dashboard_accessible(self):
+        response = self.client.get(reverse('dashboard'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Welcome to HR Dashboard")
