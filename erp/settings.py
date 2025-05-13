3245bd2b5a31fe6b91182b425c2c0f97a41616ed
adhari Dev <aryaadhari20@gmail.com>
2025-05-13 23:17:45 +0700

    fix: allow all host

--- erp/settings.py
+++ erp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-mij#m*5^!#wu%oz2ta3h#xd_^_dup2*n)h$7gu!qvr@bg!i4wf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
